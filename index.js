aadedbe2654f0ba99e0b76e11973085856fb41da
samibonzela07-sketch <samibonzela07@gmail.com>
2026-06-26 16:58:47 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits, ActivityType } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`${client.user.tag} is online!`);
+
+  client.user.setPresence({
+    activities: [
+      {
+        name: "🐉 Dragon Soul | /help",
+        type: ActivityType.Playing
+      }
+    ],
+    status: "online"
+  });
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === "ping") {
+    await interaction.reply("🏓 Pong! The Elite is online.");
+  }
+});
+
+client.login(process.env.TOKEN);
