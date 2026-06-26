3b2774d0b62b3a16b6e11977342e55060de32820
samibonzela07-sketch <samibonzela07@gmail.com>
2026-06-26 18:02:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('The Elite Bot is running smoothly!\n');
+    res.end('The Bot is running smoothly!\n');
 });
 const PORT = process.env.PORT || 10000;
 server.listen(PORT, '0.0.0.0', () => {
